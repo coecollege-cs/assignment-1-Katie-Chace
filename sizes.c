dbe62153d4e71f6681c660b1c21f4a7c9effaed3
Katie Chace <klchace@coe.edu>
2019-01-27 17:13:02 -0600

    sizes program now prints the sizes of all the data types on the list

--- sizes.c
+++ sizes.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
 int main(){
-	
+	printf("The size of int is %d", sizeof(int));
+	printf("\nThe size of char is %d", sizeof(char));
+	printf("\nThe size of long is %d", sizeof(long));
+	printf("\nThe size of float is %d", sizeof(float));
+	printf("\nThe size of double is %d", sizeof(double));
+	printf("\nThe size of unsigned int is %d", sizeof(unsigned int));
+	printf("\nThe size of unsigned char is %d", sizeof(unsigned char));
+	printf("\nThe size of unsigned long is %d", sizeof(unsigned long));
+	printf("\nThe size of int* is %d", sizeof(int*));
+	printf("\nThe size of char* is %d", sizeof(char*));
+	printf("\nThe size of long* is %d", sizeof(long*));
+
+
+
 }
